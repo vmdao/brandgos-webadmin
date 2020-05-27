90f71271b5f4d8d681523ee1e960d23df2037e97
BossNote <michaelsanino@outlook.com>
2020-05-27 18:33:40 +0700

    ad

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/ad-webadmin'),
+      dir: require('path').join(__dirname, './coverage/brandgos-webadmin'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
