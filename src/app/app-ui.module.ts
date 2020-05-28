c655798d210733a7da879dc150128490b1dbecf1
BossNote <michaelsanino@outlook.com>
2020-05-28 08:55:34 +0700

    edit package

--- src/app/app-ui.module.ts
+++ src/app/app-ui.module.ts
@@ -5,12 +5,9 @@ import vi from '@angular/common/locales/vi';
 import { AlainThemeModule } from '@delon/theme';
 import { DelonABCModule } from '@delon/abc';
 import { DelonACLModule } from '@delon/acl';
-import { AgmCoreModule } from '@agm/core';
 import { ToastrModule, ToastContainerModule } from 'ngx-toastr';
 import { NZ_I18N, vi_VN, NgZorroAntdModule } from 'ng-zorro-antd';
-// import { AmChartsModule } from '@amcharts/amcharts4/core';
 import { Angulartics2Module } from 'angulartics2';
-import { Angulartics2Mixpanel } from 'angulartics2/mixpanel';
 
 registerLocaleData(vi);
 
@@ -19,23 +16,18 @@ registerLocaleData(vi);
     ToastrModule.forRoot({
       preventDuplicates: true,
     }),
-    AgmCoreModule.forRoot({
-      apiKey: 'AIzaSyAh5sGTrjeMAEHbuzRrrqQh0l2_ot8n59w',
-    }),
     Angulartics2Module.forRoot(),
     ToastContainerModule,
     AlainThemeModule,
     DelonABCModule,
     DelonACLModule,
     NgZorroAntdModule,
-    // AmChartsModule,
   ],
   exports: [
     DelonABCModule,
     AlainThemeModule,
     DelonACLModule,
     NgZorroAntdModule,
-    AgmCoreModule,
     Angulartics2Module,
   ],
   providers: [{ provide: NZ_I18N, useValue: vi_VN }],
