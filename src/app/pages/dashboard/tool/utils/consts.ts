4a624ab7d048c511cecee05c69cad8e2d696f53c
BossNote <michaelsanino@outlook.com>
2020-09-08 12:45:53 +0700

    f

--- src/app/pages/dashboard/tool/utils/consts.ts
+++ src/app/pages/dashboard/tool/utils/consts.ts
@@ -1,19 +1,19 @@
 export const EDITOR_PROPERTIES = [
-    "memory",
-    "eventBus",
-    "keyManager",
-    "moveableData",
-    "moveableManager",
-    "historyManager",
-    "console",
+  'memory',
+  'eventBus',
+  'keyManager',
+  'moveableData',
+  'moveableManager',
+  'historyManager',
+  'console',
 ];
-export const PREFIX = "scena-";
-export const DATA_SCENA_ELEMENT_ID = "data-scena-element-id";
-export const DATA_SCENA_ELEMENT = "data-scena-element";
+export const PREFIX = 'scena-';
+export const DATA_SCENA_ELEMENT_ID = 'data-scena-element-id';
+export const DATA_SCENA_ELEMENT = 'data-scena-element';
 export const userAgent =
-    ((typeof navigator !== "undefined" && navigator) || ({} as any))
-        .userAgent || "";
+  ((typeof navigator !== 'undefined' && navigator) || ({} as any)).userAgent ||
+  '';
 export const isMacintosh =
-    userAgent.indexOf("Macintosh") >= 0 ||
-    userAgent.indexOf("iPad") >= 0 ||
-    userAgent.indexOf("iPhone") >= 0;
+  userAgent.indexOf('Macintosh') >= 0 ||
+  userAgent.indexOf('iPad') >= 0 ||
+  userAgent.indexOf('iPhone') >= 0;
