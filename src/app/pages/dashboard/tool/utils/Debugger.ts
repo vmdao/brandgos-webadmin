4fb877486a742b377e6d32745eeb324fdc104466
BossNote <michaelsanino@outlook.com>
2020-09-09 22:14:11 +0700

    add

--- src/app/pages/dashboard/tool/utils/Debugger.ts
+++ src/app/pages/dashboard/tool/utils/Debugger.ts
@@ -1,9 +1,13 @@
 export default class Debugger {
-    constructor(private isDebug?: boolean) {}
-    public log(...args: any[]) {
-        if (!this.isDebug) {
-            return;
-        }
-        console.log("%c Scena Debugger:", "padding: 1px; background: #4af; color: #fff;", ...args);
+  constructor(private isDebug?: boolean) {}
+  public log(...args: any[]) {
+    if (!this.isDebug) {
+      return;
     }
+    console.log(
+      '%c Scena Debugger:',
+      'padding: 1px; background: #4af; color: #fff;',
+      ...args
+    );
+  }
 }
